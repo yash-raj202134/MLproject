ee72091f78aa397928a9bda91d7040316fbbed85
yash-raj202134 <yashraj3376@gmail.com>
2024-03-07 14:10:17 +0530

    Model trainer update

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -48,7 +48,7 @@ class DataIngestion:
         
 
 from src.components.data_transformation import DataTransformation , DataTransformationConfig
-
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
  
 if __name__ == "__main__":
@@ -56,5 +56,7 @@ if __name__ == "__main__":
     train_data , test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr, _ = data_transformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
